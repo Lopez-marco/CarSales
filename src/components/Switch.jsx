320b8a6b182adc5bad224928a032e7b976968508
Marco Lopez <lopez.marcohdez@gmail.com>
2021-11-16 14:16:38 -0500

    test3

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -35,9 +35,8 @@ const SwitchLink = (props) => {
         <Route exact path="/inventory">
             <Dashboard website={props.website} />
         </Route>
-        <Route exact path="/test">
-        <Dashboard website={props.website} />
-            {/* <Auth updateToken={props.updateToken} /> */}
+        <Route exact path="/login">
+            <Auth updateToken={props.updateToken} />
         </Route>
         <Route exact path="/product_view/:id" component={VehicleView} />
         <Route exact path="/contacus">
