e3d03f34ecbaf8faf983e9b09e43be33bc01ddaa
Marco Lopez <lopez.marcohdez@gmail.com>
2021-11-22 10:27:10 -0500

    message

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -8,6 +8,6 @@ switch (window.location.hostname) {
     // this is the deployed react application
     case '6193f50fec227251d6ff162e--clever-curran-ffbad0.netlify.app':
         // this is the full url of your deployed API
-        APIURL = 'https://car-dealerserver.herokuapp.com'
+        APIURL = 'https://car-sales-server.herokuapp.com/'
 }
 export default APIURL;
